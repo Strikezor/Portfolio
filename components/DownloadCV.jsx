cd138557ecbf4d815375a2bf81377f3fb1577d5f
Strikezor <akshaych064@gmail.com>
2026-08-23 14:29:08 +0530

    Upadating download cv logic

--- components/DownloadCV.jsx
+++ components/DownloadCV.jsx
@@ -4,19 +4,21 @@ import { FiDownload } from "react-icons/fi";
 import { Button } from "./ui/button";
 
 const DownloadCV = () => {
-  const handleDownloadClick = () => {
-    window.open("https://akshay-chaturvedi-resume.tiiny.site");
-  };
-
   return (
     <Button
+      asChild
       variant="outline"
       size="lg"
       className="uppercase flex items-center gap-2"
-      onClick={handleDownloadClick}
     >
-      <span>Download CV</span>
-      <FiDownload className="text-xl" />
+      <a
+        href="https://akshay-chaturvedi-resume.tiiny.site"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span>Download CV</span>
+        <FiDownload className="text-xl" />
+      </a>
     </Button>
   );
 };
